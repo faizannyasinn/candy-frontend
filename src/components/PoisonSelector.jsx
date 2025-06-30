0b7cbaef96cc109a1e63cca8da3a247db76f3c2e
Faizan Ul Haq Yaseen <faizannyasinn@gmail.com>
2025-06-30 17:29:05 +0500

    Update PoisonSelector.jsx

--- src/components/PoisonSelector.jsx
+++ src/components/PoisonSelector.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import './PoisonSelector.css';
 
-function PoisonSelector({ candies, onSelect, selected }) {
+const candies = [
+  "/images/candy1.png", "/images/candy2.png", "/images/candy3.png",
+  "/images/candy4.png", "/images/candy5.png", "/images/candy6.png",
+  "/images/candy7.png", "/images/candy8.png", "/images/candy9.png",
+];
+
+function PoisonSelector({ onSelect }) {
   return (
     <div className="poison-grid">
-      {candies.map((candy, index) => (
-        <div
+      {candies.map((src, index) => (
+        <img
           key={index}
-          className={`poison-candy ${selected === index ? 'selected' : ''}`}
+          src={src}
+          alt={`Candy ${index}`}
+          className="candy-img"
           onClick={() => onSelect(index)}
-        >
-          🍭
-        </div>
+        />
       ))}
     </div>
   );
