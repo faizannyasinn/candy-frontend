3923807d4688cb2d4ba7d474dc714428cc1eea0c
Faizan Ul Haq Yaseen <faizannyasinn@gmail.com>
2025-06-30 17:03:53 +0500

    Update CandyBoard.jsx

--- src/components/CandyBoard.jsx
+++ src/components/CandyBoard.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import './CandyBoard.css';
 
-function CandyBoard({ isYourTurn, onCandyClick }) {
-  const candies = Array.from({ length: 15 });
-
+function CandyBoard({ candies, onClick }) {
   return (
-    <div>
-      <h4>{isYourTurn ? "Your Turn!" : "Opponent's Turn"}</h4>
-      <div className="candy-board">
-        {candies.map((_, i) => (
-          <div key={i} className="candy" onClick={() => isYourTurn && onCandyClick(i)}>
-            🍬
-          </div>
-        ))}
-      </div>
+    <div className="candy-grid">
+      {candies.map((candy, index) => (
+        <div key={index} className="candy-tile" onClick={() => onClick(index)}>
+          🍬
+        </div>
+      ))}
     </div>
   );
 }
