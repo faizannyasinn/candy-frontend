0d0349e27e761d1509a6536083f692c5747835ad
Faizan Ul Haq Yaseen <faizannyasinn@gmail.com>
2025-06-30 13:39:56 +0500

    Added App.jsx to complete React entry

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
+      <h1>🍬 Tutti Frutti Candy Game</h1>
+      <p>Multiplayer candy game is coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
