3bb6fe5899c709a75337ff6ae283179622c31e02
Faizan Ul Haq Yaseen <faizannyasinn@gmail.com>
2025-06-30 16:43:21 +0500

    Update CandyBoard.jsx

--- src/components/CandyBoard.jsx
+++ src/components/CandyBoard.jsx
@@ -1,25 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './CandyBoard.css';
 
-const candyImages = [
-  'c1.png', 'c2.png', 'c3.png', 'c4.png', 'c5.png',
-  'c6.png', 'c7.png', 'c8.png', 'c9.png', 'c10.png',
-  'c11.png', 'c12.png', 'c13.png', 'c14.png', 'c15.png'
-];
+function CandyBoard({ isYourTurn, onCandyClick }) {
+  const candies = Array.from({ length: 15 });
 
-function CandyBoard({ isYourTurn, onCandyClick, message }) {
   return (
-    <div className="candy-board-container">
-      <h3>{message}</h3>
-      <div className="candy-grid">
-        {candyImages.map((img, index) => (
-          <img
-            key={index}
-            src={`/candies/${img}`}
-            alt={`Candy ${index + 1}`}
-            className="candy-img"
-            onClick={() => isYourTurn && onCandyClick(index)}
-          />
+    <div>
+      <h4>{isYourTurn ? "Your Turn!" : "Opponent's Turn"}</h4>
+      <div className="candy-board">
+        {candies.map((_, i) => (
+          <div key={i} className="candy" onClick={() => isYourTurn && onCandyClick(i)}>
+            🍬
+          </div>
         ))}
       </div>
     </div>
